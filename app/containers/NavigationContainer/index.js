f9dccb9eacbd31c6e5bed0b0de1ea231e308f0db
Seda Bulut <SBulut@scottlogic.com>
2017-12-22 17:16:31 +0000

    Loading data from server via redux saga

---
+++ app/containers/NavigationContainer/index.js
@@ -0,0 +1,40 @@
+/*
+ *
+ * NavigationContainer
+ *
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import selectNavigationContainer from './selectors';
+import Navigation from '../../components/Navigation'
+import { requestTopics } from './actions'
+
+export class NavigationContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    requestTopics: React.PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    this.props.requestTopics()
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation {...this.props} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = selectNavigationContainer();
+
+function mapDispatchToProps(dispatch) {
+  return {
+    requestTopics: () => dispatch(requestTopics()), 
+    selectTopic: (t) => console.log('selected yo', t)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationContainer);
---
+++ app/containers/NavigationContainer/reducer.js
@@ -0,0 +1,23 @@
+/*
+ *
+ * NavigationContainer reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import { REQUEST_TOPICS, REQUEST_TOPICS_SUCCEEDED } from './constants';
+
+const initialState = fromJS({
+  topics: []
+});
+
+function navigationContainerReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_TOPICS_SUCCEEDED: 
+    return state.set('topics', action.topics)
+    default:
+      return state;
+  }
+}
+
+export default navigationContainerReducer;
